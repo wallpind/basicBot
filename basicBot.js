3ee6c680374a2c0cf1262b6dbebda33efd4161e1
Nick Yahnke <nthitz@gmail.com>
2015-03-04 10:49:49 -0800

    bot dance
    i didn't test this at all

--- basicBot.js
+++ basicBot.js
@@ -1621,7 +1621,18 @@
                     }
                 }
             },
-
+	    danceCommand: {
+                command: 'dance',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        $('#woot').click()
+                    }
+                }
+            },
             cookieCommand: {
                 command: 'cookie',
                 rank: 'user',
